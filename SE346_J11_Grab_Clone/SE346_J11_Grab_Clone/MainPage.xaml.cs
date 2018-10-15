054f1c82fa68fce24a7793126ad1c9acaf821d8a
NhoxLeo <37131285+NhoxLeo@users.noreply.github.com>
2018-10-15 07:55:35 +0700

    Add project files.

---
+++ SE346_J11_Grab_Clone/SE346_J11_Grab_Clone/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SE346_J11_Grab_Clone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
